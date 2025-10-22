e0ced04bc630ffc2249f210a045ee862db11c83c
zeth1z <golittleh1z@gmail.com>
2025-10-22 10:22:22 +0300

    Первая версия программы

---
+++ work7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace rrr;
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа для нахождения периметра, полупериметра и площади треугольника по трём его сторонам");
+    }
+
+    public static double CalculateTriangleArea(double a, double b, double c)
+    {
+        try
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                throw new ArgumentException("Треугольника с такими сторонами не бывает");
+            }
+
+            double P = a + b + c;
+            double p = P / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            return area;
+        }
+        catch (ArgumentException ex)
+        {
+            throw new ArgumentException($"Ошибка при вычислении площади: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Математическая ошибка: {ex.Message}");
+        }
+    }
+}

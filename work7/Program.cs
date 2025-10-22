80234eb7f0a75e79702129ffe342019af8ddd87e
zeth1z <golittleh1z@gmail.com>
2025-10-22 10:42:34 +0300

    Update Program.cs чтобы потом можно было слить в одну ветку
    Обновленная версия main

--- work7/Program.cs
+++ work7/Program.cs
@@ -1,8 +1,20 @@
-﻿using System;
-namespace rrr;
+using System;
 public class Program
 {
     static void Main()
     {
         Console.WriteLine("Программа для нахождения периметра, полупериметра и площади треугольника по трём его сторонам");
+        try
+        {
+            Console.Write("Введите длину стороны a: ");
+            double a = double.Parse(Console.Readline());
+            Console.Write("Введите длину стороны b: ");
+            double b = double.Parse(Console.Readline());
+            Console.Write("Введите длину стороны c: ");
+            double c = double.Parse(Console.Readline());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка, введите числовые значения для сторон треугольника");
+        }
     }
